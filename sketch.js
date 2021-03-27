5092d4d21a7d36533a22166e99b26cadc3c29cfd
SarayuNidjelli <74332513+SarayuNidjelli@users.noreply.github.com>
2021-03-27 17:56:01 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,92 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(400, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(400, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(400, 650, 600,10);
+	groundSprite.shapeColor='purple'
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(400 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(400, 650, 800, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+
+ 	boxleftSprite=createSprite(300, 600, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(320, 600, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+
+
+ 	boxBase=createSprite(400, 640, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(400, 625, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+
+
+ 	boxleftSprite=createSprite(500 , 600, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(480 , 600, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ 
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  
+  drawSprites();
+  
+  
+ 
+}
+
+
+function keyPressed() {
+    if (keyCode === DOWN_ARROW) {
+      Matter.Body.setStatic(packageBody,false);
+       
+     }
+}
